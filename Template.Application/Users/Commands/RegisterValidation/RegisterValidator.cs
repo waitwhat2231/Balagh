82456b423f0f4799cd834afa472b9f9808e8f36f
MajdArous <somethingsomeone036+12@gmail.com>
2025-11-15 03:09:34 +0300

    Validator Temporary Change

--- Template.Application/Users/Commands/RegisterValidation/RegisterValidator.cs
+++ Template.Application/Users/Commands/RegisterValidation/RegisterValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Template.Domain.Enums;
 
 namespace Template.Application.Users.Commands.RegisterValidation
 {
@@ -6,20 +7,19 @@ namespace Template.Application.Users.Commands.RegisterValidation
     {
         public RegisterValidator()
         {
-            When(x => x.Role == "user", () =>
+            When(x => x.Role.ToUpper() == nameof(EnumRoleNames.User).ToUpper(), () =>
             {
                 RuleFor(x => x.GovernmentalEntityId)
                     .Null()
                     .WithMessage(" Govermental entity must be null when role is 'user'.");
             });
 
-            // Id MUST NOT be null when Role is NOT "user"
-            When(x => x.Role != "user", () =>
-            {
-                RuleFor(x => x.GovernmentalEntityId)
-                    .NotNull()
-                    .WithMessage("Governmental entity is required when role is not 'user'.");
-            });
+            //When(x => x.Role != "user", () =>
+            //{
+            //    RuleFor(x => x.GovernmentalEntityId)
+            //        .NotNull()
+            //        .WithMessage("Governmental entity is required when role is not 'user'.");
+            //});
 
         }
     }
