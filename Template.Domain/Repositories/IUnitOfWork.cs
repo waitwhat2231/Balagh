0257d5ad0028b2b2ed5b5856916a7b96c3193382
MajdArous <somethingsomeone036+12@gmail.com>
2025-12-29 00:53:53 +0300

    Iunitofwork added

---
+++ Template.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Template.Domain.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task BeginTransactionAsync();
+        Task CommitAsync();
+        Task RollbackAsync();
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ Template.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Template.Domain.Repositories;
+using Template.Infrastructure.Persistence;
+
+namespace Template.Infrastructure.Repositories
+{
+    class UnitOfWork(TemplateDbContext dbContext, IDbContextTransaction? dbContextTransaction) : IUnitOfWork
+    {
+        public async Task BeginTransactionAsync()
+        {
+            await dbContext.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitAsync()
+        {
+            try
+            {
+                await dbContext.SaveChangesAsync();
+                await dbContextTransaction?.CommitAsync();
+            }
+            catch
+            {
+                await RollbackAsync();
+                throw;
+            }
+            finally
+            {
+                Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            dbContextTransaction?.Dispose();
+            dbContextTransaction = null;
+        }
+
+        public async Task RollbackAsync()
+        {
+            if (dbContextTransaction != null)
+                await dbContextTransaction.RollbackAsync();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await dbContext.SaveChangesAsync();
+        }
+    }
+}
